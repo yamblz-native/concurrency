b9e27d6754054591b287bab714d20bab6c52f7f6
Valeriy Shtaihts <bajiepkasmile@yandex.ru>
2016-07-14 16:39:10 +0300

    concurrency homework

--- app/src/main/java/ru/yandex/yamblz/concurrency/LoadProducer.java
+++ app/src/main/java/ru/yandex/yamblz/concurrency/LoadProducer.java
@@ -3,6 +3,7 @@ package ru.yandex.yamblz.concurrency;
 import android.support.annotation.NonNull;
 
 import java.util.Set;
+import java.util.concurrent.CountDownLatch;
 
 /**
  * Simple load producer thread; non-extensible
@@ -14,21 +15,26 @@ public final class LoadProducer extends Thread {
 
     @NonNull private final Set<String> results;
     @NonNull private final Runnable onResult;
+    private CountDownLatch countDownLatch;
 
-    public LoadProducer(@NonNull Set<String> resultSet, @NonNull Runnable onResult) {
+    public LoadProducer(@NonNull Set<String> resultSet,
+                        @NonNull Runnable onResult,
+                        CountDownLatch countDownLatch) {
         this.results = resultSet;
         this.onResult = onResult;
+        this.countDownLatch = countDownLatch;
     }
 
     @Override
     public void run() {
         super.run();
 
-        /* Synchronize via concurrent mechanics */
-
         final String result = new DownloadLatch().doWork();
-        results.add(result);
-
+        synchronized (results){
+            results.add(result);
+        }
         onResult.run();
+
+        countDownLatch.countDown();
     }
 }
