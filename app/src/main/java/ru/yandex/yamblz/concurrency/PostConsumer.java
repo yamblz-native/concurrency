216a8fa1d86ad590eca76fb7ebc5265308a738e3
Valentin Postnov <valpostnov@gmail.com>
2016-07-15 00:31:06 +0300

    hw

--- app/src/main/java/ru/yandex/yamblz/concurrency/PostConsumer.java
+++ app/src/main/java/ru/yandex/yamblz/concurrency/PostConsumer.java
@@ -2,6 +2,8 @@ package ru.yandex.yamblz.concurrency;
 
 import android.support.annotation.NonNull;
 
+import java.util.concurrent.CountDownLatch;
+
 /**
  * Simple result consumer thread; non-extensible
  *
@@ -11,17 +13,26 @@ import android.support.annotation.NonNull;
 public final class PostConsumer extends Thread {
 
     @NonNull private final Runnable onFinish;
+    private final CountDownLatch countDownLatch;
 
-    public PostConsumer(@NonNull Runnable onFinish) {
+    public PostConsumer(@NonNull Runnable onFinish, CountDownLatch countDownLatch) {
         this.onFinish = onFinish;
+        this.countDownLatch = countDownLatch;
     }
 
     @Override
     public void run() {
         super.run();
 
-        /* Synchronize via concurrent mechanics */
+        try
+        {
+            countDownLatch.await();
+        }
+        catch (InterruptedException e)
+        {
+            e.printStackTrace();
+        }
 
-        onFinish.run();
+        new Thread(onFinish).start();
     }
 }
--- app/src/main/java/ru/yandex/yamblz/ui/fragments/ContentFragment.java
+++ app/src/main/java/ru/yandex/yamblz/ui/fragments/ContentFragment.java
@@ -10,6 +10,7 @@ import android.widget.TextView;
 
 import java.util.LinkedHashSet;
 import java.util.Set;
+import java.util.concurrent.CountDownLatch;
 
 import butterknife.BindView;
 import ru.yandex.yamblz.R;
@@ -21,37 +22,62 @@ public class ContentFragment extends BaseFragment {
 
     private static final String CONSUME_EXCEPTION = "Some producers not finished yet!";
     private static final int PRODUCERS_COUNT = 5;
+    private boolean producersIsExist;
 
     @BindView(R.id.hello) TextView helloView;
 
     @NonNull private final Set<String> dataResults = new LinkedHashSet<>();
 
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setRetainInstance(true);
+    }
+
     @NonNull
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle bundle)
+    {
         return inflater.inflate(R.layout.fragment_content, container, false);
     }
 
     @Override
-    public void onResume() {
+    public void onResume()
+    {
         super.onResume();
-        new PostConsumer(this::postFinish).start();
-        for (int i = 0; i < PRODUCERS_COUNT; i++) {
-            new LoadProducer(dataResults, this::postResult);
+
+        if (!producersIsExist)
+        {
+            dataResults.clear();
+            CountDownLatch countDownLatch = new CountDownLatch(PRODUCERS_COUNT);
+
+            new PostConsumer(this::postFinish, countDownLatch).start();
+
+            for (int i = 0; i < PRODUCERS_COUNT; i++)
+            {
+                new LoadProducer(dataResults, this::postResult, countDownLatch).start();
+            }
+
+            producersIsExist = true;
         }
     }
 
-    final void postResult() {
-        assert helloView != null;
-        helloView.setText(String.valueOf(dataResults.size()));
+    final void postResult()
+    {
+        runOnUiThreadIfFragmentAlive(() -> helloView.setText(String.valueOf(dataResults.size())));
     }
 
-    final void postFinish() {
-        if (dataResults.size() < PRODUCERS_COUNT) {
-            throw new RuntimeException(CONSUME_EXCEPTION);
-        }
+    final void postFinish()
+    {
+        runOnUiThreadIfFragmentAlive(() ->
+        {
+            if (dataResults.size() < PRODUCERS_COUNT) {
+                throw new RuntimeException(CONSUME_EXCEPTION);
+            }
+            helloView.setText(R.string.task_win);
+        });
 
-        assert helloView != null;
-        helloView.setText(R.string.task_win);
+        producersIsExist = false;
     }
 }
