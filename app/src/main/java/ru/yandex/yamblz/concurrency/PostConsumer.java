a9456d76c5b172c7c4bfa7f451e1e6de6bc94ae9
Danil Yaschenko <danyaschenko@gmail.com>
2016-07-14 04:33:07 +0300

    concurrency homework

--- app/src/main/java/ru/yandex/yamblz/concurrency/PostConsumer.java
+++ app/src/main/java/ru/yandex/yamblz/concurrency/PostConsumer.java
@@ -2,6 +2,9 @@ package ru.yandex.yamblz.concurrency;
 
 import android.support.annotation.NonNull;
 
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CyclicBarrier;
+
 /**
  * Simple result consumer thread; non-extensible
  *
@@ -11,17 +14,26 @@ import android.support.annotation.NonNull;
 public final class PostConsumer extends Thread {
 
     @NonNull private final Runnable onFinish;
-
-    public PostConsumer(@NonNull Runnable onFinish) {
-        this.onFinish = onFinish;
-    }
+    private CyclicBarrier barrier;
 
     @Override
     public void run() {
         super.run();
 
         /* Synchronize via concurrent mechanics */
+        try {
+            barrier.await();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (BrokenBarrierException e) {
+            e.printStackTrace();
+        }
 
         onFinish.run();
+    }
+
+    public PostConsumer(@NonNull Runnable onFinish, CyclicBarrier barrier) {
+        this.onFinish = onFinish;
+        this.barrier = barrier;
     }
 }
