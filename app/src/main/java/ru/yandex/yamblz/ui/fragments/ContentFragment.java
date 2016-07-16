21603bc2cc0def5940c4e1fe64d12d642fa08055
Sergey Atroshchenko <sergey.atroshchenko@yandex.ru>
2016-07-16 12:39:46 +0300

    add conccurentcy collection

--- app/src/main/java/ru/yandex/yamblz/ui/fragments/ContentFragment.java
+++ app/src/main/java/ru/yandex/yamblz/ui/fragments/ContentFragment.java
@@ -5,14 +5,13 @@ import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
-import java.util.LinkedHashSet;
 import java.util.Set;
+import java.util.concurrent.ConcurrentSkipListSet;
 import java.util.concurrent.Phaser;
 
 import butterknife.BindView;
@@ -29,7 +28,8 @@ public class ContentFragment extends BaseFragment {
 
     @BindView(R.id.hello) TextView helloView;
 
-    @NonNull private final Set<String> dataResults = new LinkedHashSet<>();
+    @NonNull
+    private final Set<String> dataResults = new ConcurrentSkipListSet<>();
 
     @NonNull
     @Override
